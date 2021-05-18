2bd1e8c8cd3684a082c78c2e92b98a5c59d0115f
ec3566 <47432136+ec3566@users.noreply.github.com>
2021-05-17 22:40:42 -0400

    Day 1

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/json/ConfigurationLoader.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/json/ConfigurationLoader.java
@@ -1,7 +1,15 @@
 package com.udacity.webcrawler.json;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import java.io.IOException;
 import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.Objects;
 
 /**
@@ -9,37 +17,47 @@ import java.util.Objects;
  */
 public final class ConfigurationLoader {
 
-  private final Path path;
+	private final Path path;
 
-  /**
-   * Create a {@link ConfigurationLoader} that loads configuration from the given {@link Path}.
-   */
-  public ConfigurationLoader(Path path) {
-    this.path = Objects.requireNonNull(path);
-  }
+	/**
+	 * Create a {@link ConfigurationLoader} that loads configuration from the given {@link Path}.
+	 */
+	public ConfigurationLoader ( Path path ) {
+		this.path = Objects.requireNonNull ( path );
+	}
 
-  /**
-   * Loads configuration from this {@link ConfigurationLoader}'s path
-   *
-   * @return the loaded {@link CrawlerConfiguration}.
-   */
-  public CrawlerConfiguration load() {
-    // TODO: Fill in this method.
+	/**
+	 * Loads configuration from this {@link ConfigurationLoader}'s path
+	 *
+	 * @return the loaded {@link CrawlerConfiguration}.
+	 */
+	@JsonDeserialize ( builder = CrawlerConfiguration.Builder.class )
+	public CrawlerConfiguration load ( ) {
+      System.out.println ( "Method load ( ) start..." );
+      try {
+        Reader reader = Files.newBufferedReader ( path, StandardCharsets.UTF_8 );
+        this.read ( reader );
+        reader.close ( );
+      } catch ( IOException e ) {
+        e.printStackTrace ( );
+      }
 
-    return new CrawlerConfiguration.Builder().build();
-  }
+      return new CrawlerConfiguration.Builder ( ).build ( );
+	}
 
-  /**
-   * Loads crawler configuration from the given reader.
-   *
-   * @param reader a Reader pointing to a JSON string that contains crawler configuration.
-   * @return a crawler configuration
-   */
-  public static CrawlerConfiguration read(Reader reader) {
-    // This is here to get rid of the unused variable warning.
-    Objects.requireNonNull(reader);
-    // TODO: Fill in this method
+	/**
+	 * Loads crawler configuration from the given reader.
+	 *
+	 * @param reader a Reader pointing to a JSON string that contains crawler configuration.
+	 * @return a crawler configuration
+	 */
 
-    return new CrawlerConfiguration.Builder().build();
-  }
+	public static CrawlerConfiguration read ( Reader reader ) throws IOException {
+		// This is here to get rid of the unused variable warning.
+		Objects.requireNonNull ( reader );
+		ObjectMapper objMapper = new ObjectMapper (  );
+		objMapper.disable ( JsonParser.Feature.AUTO_CLOSE_SOURCE );
+
+		return objMapper.readValue ( reader, CrawlerConfiguration.Builder.class ).build ( );
+	}
 }
--- starter/webcrawler/src/main/java/com/udacity/webcrawler/json/CrawlResultWriter.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/json/CrawlResultWriter.java
@@ -1,6 +1,12 @@
 package com.udacity.webcrawler.json;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
 import java.io.Writer;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
 
@@ -8,37 +14,50 @@ import java.util.Objects;
  * Utility class to write a {@link CrawlResult} to file.
  */
 public final class CrawlResultWriter {
-  private final CrawlResult result;
+	private final CrawlResult result;
 
-  /**
-   * Creates a new {@link CrawlResultWriter} that will write the given {@link CrawlResult}.
-   */
-  public CrawlResultWriter(CrawlResult result) {
-    this.result = Objects.requireNonNull(result);
-  }
+	/**
+	 * Creates a new {@link CrawlResultWriter} that will write the given {@link CrawlResult}.
+	 */
+	public CrawlResultWriter ( CrawlResult result ) {
+		this.result = Objects.requireNonNull ( result );
+	}
 
-  /**
-   * Formats the {@link CrawlResult} as JSON and writes it to the given {@link Path}.
-   *
-   * <p>If a file already exists at the path, the existing file should not be deleted; new data
-   * should be appended to it.
-   *
-   * @param path the file path where the crawl result data should be written.
-   */
-  public void write(Path path) {
-    // This is here to get rid of the unused variable warning.
-    Objects.requireNonNull(path);
-    // TODO: Fill in this method.
-  }
+	/**
+	 * Formats the {@link CrawlResult} as JSON and writes it to the given {@link Path}.
+	 *
+	 * <p>If a file already exists at the path, the existing file should not be deleted; new data
+	 * should be appended to it.
+	 *
+	 * @param path the file path where the crawl result data should be written.
+	 */
+	public void write ( Path path ) throws IOException {
+		// This is here to get rid of the unused variable warning.
+		Objects.requireNonNull ( path );
+		ObjectMapper objMapper = new ObjectMapper ( );
+		File file = new File ( String.valueOf ( path ) );
+		try {
+			objMapper.writeValue ( file, result );
+		} catch ( IOException e ) {
+			e.printStackTrace ( );
+		}
 
-  /**
-   * Formats the {@link CrawlResult} as JSON and writes it to the given {@link Writer}.
-   *
-   * @param writer the destination where the crawl result data should be written.
-   */
-  public void write(Writer writer) {
-    // This is here to get rid of the unused variable warning.
-    Objects.requireNonNull(writer);
-    // TODO: Fill in this method.
-  }
+	}
+
+	/**
+	 * Formats the {@link CrawlResult} as JSON and writes it to the given {@link Writer}.
+	 *
+	 * @param writer the destination where the crawl result data should be written.
+	 */
+	public void write ( Writer writer ) {
+		// This is here to get rid of the unused variable warning.
+		Objects.requireNonNull ( writer );
+		ObjectMapper objMapper = new ObjectMapper ( );
+		objMapper.disable ( JsonGenerator.Feature.AUTO_CLOSE_TARGET );
+		try {
+			objMapper.writeValue ( writer, result );
+		} catch ( IOException e ) {
+			e.printStackTrace ( );
+		}
+	}
 }
